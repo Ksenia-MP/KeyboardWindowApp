83291401ad5d1536583e94612725888e492b797d
Ksenia-MP <kseniamorozova90601@gmail.com>
2022-10-15 19:28:30 +0400

    Add test login

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.MySqlClient;
+
 namespace KeyboardWIndowApp
 {
     public partial class Form1 : Form
@@ -15,6 +17,26 @@ namespace KeyboardWIndowApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // строка подключения к БД
+            string connStr = "server=sql11.freesqldatabase.com;user=sql11526749;database=sql11526749;password=7gVU827Kqz;";
+            // создаём объект для подключения к БД
+            MySqlConnection conn = new MySqlConnection(connStr);
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            string sql = "SELECT login FROM User WHERE id = 1";
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            // выполняем запрос и получаем ответ
+            string name = command.ExecuteScalar().ToString();
+            // выводим ответ в консоль
+            MessageBox.Show(name);
+            // закрываем соединение с БД
+            conn.Close();
         }
     }
 }
